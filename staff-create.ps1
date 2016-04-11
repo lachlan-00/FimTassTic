565e06fe7c649d5a79d88fd99bacf2512c7ff2f4
lachlan-00 <lachlan.00@gmail.com>
2016-04-11 10:25:48 +1000

    Revert "make sure userpass intead of randompass"
    This reverts commit 9d10c5178ef1823c88d1b4c559d822ea697708ce.

--- staff-create.ps1
+++ staff-create.ps1
@@ -312,7 +312,7 @@ foreach($line in $input) {
 
 Full Name: ${FullName}
 User Name: ${LoginName}
-Password: ${userpass}
+Password: ${randompass}
 Teacher Code: ${TeacherCode}
 Position: ${Position}
 Phone Ext: ${Telephone}
@@ -337,7 +337,7 @@ This is an automated email.
 
 Full Name: ${FullName}
 User Name: ${LoginName}
-Password: ${userpass}
+Password: ${randompass}
 Teacher Code: ${TeacherCode}
 Position: ${Position}
 Phone Ext: ${Telephone}
